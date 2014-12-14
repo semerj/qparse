2a41c40e9bbd33f394faa706a6728c6ef4f7f807
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-12-13 17:51:44 -0800

    include corenlp library to call stanford parser on the fly
    - it works but it's hella slow

--- flask/qparse.py
+++ flask/qparse.py
@@ -1,5 +1,8 @@
+from corenlp import corenlp
 from flask import Flask, render_template, request
 import json
+import jsonrpclib
+import subprocess
 
 app = Flask(__name__)
 
@@ -20,6 +23,24 @@ def input_form():
     if request.method == "POST":
         # process the story
         # from magic import do.everything
+        storytext = request.form['storytext']
+        # write it to a text file to feed to the parser
+        with open('upload/temp', 'w') as outfile:
+            outfile.write(storytext.encode('utf8'))
+
+        # TODO: this is slow af
+        parsed = corenlp.batch_parse('upload/', 'corenlp-python/javaparser/')
+
+        # there will only ever be one file to parse
+        parsed = next(parsed)
+        # print parsed
+
+        # assume there's already a stanfordnlp server running
+        # server = jsonrpclib.Server("http://127.0.0.1:8080")
+        # return server.parse(storytext)
+        # TODO: too slow, it just stalls out without erroring
+
+        # return 'see file'
 
         #display the results
         with open('out.json', 'r') as infile:
